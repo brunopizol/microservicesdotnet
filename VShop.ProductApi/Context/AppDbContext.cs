ee7c2ddb7ea51bd09572b3b391915aae2fb5fdda
Bruno Pizol Camargo <bcamargo@topsys.com>
2022-08-16 17:54:57 -0300

    dbcontext format file

--- VShop.ProductApi/Context/AppDbContext.cs
+++ VShop.ProductApi/Context/AppDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using VShop.ProductApi.Models;
 
 namespace VShop.ProductApi.Context
 {
     class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
