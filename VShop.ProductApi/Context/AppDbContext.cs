652add2342e8844e9848626a358f7aa4993c5704
Bruno Pizol Camargo <bcamargo@topsys.com>
2022-08-17 13:56:55 -0300

    fluent API and migrations

--- VShop.ProductApi/Context/AppDbContext.cs
+++ VShop.ProductApi/Context/AppDbContext.cs
@@ -9,5 +9,40 @@ namespace VShop.ProductApi.Context
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        //Fluent API
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            //category
+            mb.Entity<Category>().HasKey(c => c.CategoryId);
+            mb.Entity<Category>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+
+            //Product
+            mb.Entity<Product>().Property(c => c.Name).HasMaxLength(100).IsRequired();
+            mb.Entity<Product>().Property(c => c.Description).HasMaxLength(255).IsRequired();
+            mb.Entity<Product>().Property(c => c.ImageURL).HasMaxLength(255).IsRequired();
+            mb.Entity<Product>().Property(c => c.Price).HasPrecision(12, 2);
+
+            //relacionamentos
+            mb.Entity<Category>()
+                .HasMany(g => g.Products)
+                .WithOne(c => c.Category)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            //populando registros
+            mb.Entity<Category>().HasData(
+                new Category{
+                    CategoryId=1,
+                    Name = "Material Escolar",
+                },
+                new Category{
+                    CategoryId=2,
+                    Name = "Acessorios",
+
+                }
+            );
+
+        }
     }
 }
