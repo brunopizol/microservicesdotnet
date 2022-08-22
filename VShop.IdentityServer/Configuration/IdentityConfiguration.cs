00fdd0c8dfa9bb2f8fe87d63c09006b567b14fbd
brunopizol <brunopizolcamargo.contato@gmail.com>
2022-08-21 22:50:03 -0300

    configuring identityserver

---
+++ VShop.IdentityServer/Configuration/IdentityConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace VShop.IdentityServer.Configuration
+{
+    public class IdentityConfiguration
+    {
+        public const string Admin = "Admin";
+        public const string Client = "Client";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("vshop", "VShop Server"),
+                new ApiScope(name:"read", "read Data."),
+                new ApiScope(name:"write", "write data."),
+                new ApiScope(name: "delete", "delete data."),
+            };
+
+        public static IEnumerable<Client> Clients =>
+      new List<Client>
+      {
+               //cliente genérico
+                new Client
+                {
+                    ClientId = "client",
+                    ClientSecrets = { new Secret("abracadabra#simsalabim".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials, //precisa das credenciais do usuário
+                    AllowedScopes = {"read", "write", "profile" }
+                },
+                new Client
+                {
+                    ClientId = "vshop",
+                    ClientSecrets = { new Secret("abracadabra#simsalabim".Sha256())},
+                    AllowedGrantTypes = GrantTypes.Code, //via codigo
+                    RedirectUris = {"https://localhost:7165/signin-oidc"},//login
+                    PostLogoutRedirectUris = {"https://localhost:7165/signout-callback-oidc"},//logout
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "vshop"
+                    }
+                }
+      };
+    }
+}
